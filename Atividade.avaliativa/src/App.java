38af41c6c80e83558844c51906c05e5bcab488d7
João Pedro Dos Santos Oliveira Carneiro <joaopedrocarneiro01@gmail.com>
2026-08-30 16:57:11 +0000

    atividade-avaliativa

--- Atividade.avaliativa/src/App.java
+++ Atividade.avaliativa/src/App.java
@@ -57,13 +57,81 @@ public class App {
         int nota1 = 35;
         int nota2 = 45;
         double nota3 = 45.7;
-        double resultado = nota1 + nota2 + nota3 / 3;
+        double resultado = (nota1 + nota2 + nota3) / 3;
 
         System.out.println("Nota 1: " + nota1 );
         System.out.println("Nota 2: " + nota2 );
         System.out.println("Nota 3: " + nota3 );
         System.out.println("Media: " + resultado );
         
+        System.out.println("Exercicio 07");
+
+        String nomedoproduto = "Panela";
+        int precounitario1 = 240;
+        int quantidavendida = 40;
+        int valortotal = quantidavendida*precounitario1;
+
+        System.out.println("Preço unitario: " + precounitario1);
+        System.out.println("Quantidade Vendida: " + quantidavendida);
+        System.out.println("Valor total: " + valortotal);
+
+        System.out.println("Exercicio 08");
+
+        String nomedofuncionario = "Miguel";
+        double valordahora = 6.62;
+        int horastrabalhada = 300;
+        double valordosalario = valordahora*horastrabalhada;
+
+        System.out.println("Nome do Funcionario: " + nomedofuncionario);
+        System.out.println("Valor da hora: " + valordahora);
+        System.out.println("Horas trabalhadas: " + horastrabalhada);
+        System.out.println("Valor do salario bruto: " + valordosalario);
+
+        System.out.println("Exercicio 09");
+
+        double celsius = 42.2;
+        double fahrenheit = celsius * 9.0 / 5.0 + 32;
+        
+        System.out.println("Temperatura: " + celsius);
+        System.out.println("Fehrenheit: " + fahrenheit);
+
+        System.out.println("Exercicio 10");
+
+        String nome02 = "Nakata";
+        String classe = "Explorador";
+        int nivel = 590;
+        int pontosdevida = 408;
+        int moedas = 1;
+        boolean status = true;
+        
+        String classeatual = "Guerreiro";
+        int nivelatual = 600;
+        int pontosdevidaatual = 350;
+        int moedasatual = 500;
+        boolean statusatual = false;
+    
+        System.out.println("Nome do personagem: " + nome02 );
+        System.out.println("Classe: " + classe );
+        System.out.println("Nivel: " + nivel );
+        System.out.println("Pontos de vida: " + pontosdevida);
+        System.out.println("Moedas: " + moedas );
+        System.out.println("Status: " + status);
+
+        System.out.println("Apos uma longa batalha com o chef final esse são os status do nosso Nakata");
+
+        System.out.println("Nome do personagem: " + nome02 );
+        System.out.println("Classe Atual: " + classeatual );
+        System.out.println("Nivel Atual: " + nivelatual );
+        System.out.println("Pontos de vida atual: " + pontosdevidaatual );
+        System.out.println("Moedas atuais: " + moedasatual);
+        System.out.println("Status: " + statusatual);
+
+
+
+
+
+
+
         
     }
 }
